e744f78f3c442a2bd9d371998579bca7051a7939
sanjay <sanjay.singh@3pillarglobal.com>
2016-11-21 15:05:51 +0530

    aadsdacc

--- ExampleForm.java
+++ ExampleForm.java
@@ -20,4 +20,33 @@ public class ExampleForm extends ActionForm {
 	public void setAge(Integer age) {
 		this.age = age;
 	}
+	
+	public ActionErrors validate(ActionMapping mapping,
+			HttpServletRequest request) {
+
+		// create a new instance of actionerrors
+		ActionErrors actionErrors = new ActionErrors();
+
+		// valdiate name
+		if (name.length() < 3) {
+			actionErrors.add("name", new ActionMessage("error.name"));
+		}
+
+		// validate age
+		if (age == null || age < 18) {
+			actionErrors.add("age", new ActionMessage("error.age"));
+		}
+
+		// return collection of action messages
+		return actionErrors;
+	}
+
+	public void reset(ActionMapping mapping, HttpServletRequest request) {
+
+		// reset properties
+		name = "";
+		age = 0;
+
+	}
+	
 }
