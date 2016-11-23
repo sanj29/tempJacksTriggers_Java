81822e29e0d219e024f3ae14eac6262c0e552a39
sanjay <sanjay.singh@3pillarglobal.com>
2016-11-23 16:37:03 +0530

    fist commit

--- StaticVector.java
+++ StaticVector.java
@@ -1,7 +1,14 @@
-public class StaticIV {
-    public static void encryptIvNotInitialize1(String message) throws Exception {
+package com.codiscope.jaks.triggers.java.staticVI;
 
-        byte[] iv = new byte[];
-        IvParameterSpec ivSpec = new IvParameterSpec(iv);
-    }
+import javax.crypto.spec.IvParameterSpec;
+
+public class StaticVector {
+
+	private static byte[] IV = new byte[4];
+	//(byte)0,(byte)1,(byte)2,(byte)3};
+
+	public void encrypt(String message) throws Exception {
+	    IvParameterSpec ivSpec = new IvParameterSpec(IV);
+	    
+}
 }
