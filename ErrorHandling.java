aa270eb277a6f006ec3a4aebec5f0ca315d98689
sanjay <sanjay.singh@3pillarglobal.com>
2016-12-01 12:44:19 +0530

    demo

--- ErrorHandling.java
+++ ErrorHandling.java
@@ -1,5 +1,4 @@
 package com.codiscope.jaks.triggers.java.ErrorPage;
-
 import javax.servlet.Servlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -7,24 +6,6 @@ import javax.servlet.jsp.JspFactory;
 import javax.servlet.jsp.JspWriter;
 import javax.servlet.jsp.PageContext;
 
-/*
- Rule:
- <Rule id="CIGITAL-JAVA-ERROR-HANDLING" lang="java">
- <Category>Error Handling</Category>
- <Title>Error Page</Title>
- <Description>Specify error page so that an application can reveal as little information as possible.</Description>
- <Match>
- <QualifiedName>javax.servlet.jsp.JspFactory</QualifiedName>
- <Method>getPageContext</Method>
- <Argument value="null" comparator="equals" type="String">3</Argument>
- </Match>
- <Standards>
- <Standard file="java-error-handling-page-context.xml">
- <Context>J2EE</Context>
- </Standard>
- </Standards>
- </Rule>
- */
 public class ErrorHandling {
 	HttpServletRequest request = null;
 
