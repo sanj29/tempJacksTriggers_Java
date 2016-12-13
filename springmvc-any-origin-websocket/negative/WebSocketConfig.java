ed925b603217874888cf95a8f0cd4cb53a949cdb
sanjay <sanjay.singh@3pillarglobal.com>
2016-12-13 13:47:51 +0530

    spring code

---
+++ springmvc-any-origin-websocket/negative/WebSocketConfig.java
@@ -0,0 +1,22 @@
+//Negative test for registering permissive allowed-origins for WebSocketHandlerRegistry class
+
+package com.programcreek.helloworld.controller;
+
+import org.springframework.context.annotation.*;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.*;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    //calling setAllowedOrigins on WebSocketHandlerRegistry instance
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+	registry.addHandler(myHandler(), "/myFoo").setAllowedOrigins("*").withSockJS();
+    }
+
+    @Bean
+    public WebSocketHandler myHandler() {
+        return new MyHandler();
+    }
+}
