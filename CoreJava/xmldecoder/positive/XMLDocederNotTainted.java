533050ee0f3e4653fe4a0637b5d025a3ee97a48a
sanjay <sanjay.singh@3pillarglobal.com>
2016-12-20 17:03:02 +0530

    java and js

--- CoreJava/xmldecoder/positive/XMLDocederNotTainted.java
+++
@@ -1,9 +0,0 @@
-class XMLDecoderNotTainted
-{
-    public static void main(String args[], Request request)
-    {
-	XMLDecoder d = new XMLDecoder("I am static");
-	Object result = d.readObject();
-	d.close();
-    }
-}
